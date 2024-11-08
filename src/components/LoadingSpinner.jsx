d0f00b9f8ca4d2de73f2d5d7ec24cc11a00cfb83
Steve Mayne <steve@doccoms.co.uk>
2024-11-08 15:08:16 +0000

    Updates

--- src/components/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoadingSpinner() {
-    return (
-        <div className="flex items-center justify-center h-screen w-screen">
-            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900">
-            </div>
-        </div>
-    );
-}
